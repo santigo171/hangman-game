9a9befe0b2f61dc32f75425fc9cae3ac7c754263
David Hurtado <santibetancourt17@gmail.com>
2021-03-16 15:43:10 -0500

    Assets added.

---
+++ main.js
@@ -0,0 +1,9 @@
+// The hangman app. in this app:
+// - play the hangman
+// - create own hangman
+// - save hangman games in local storage
+// - share hangman games with django
+
+const presentation = `
+
+`
