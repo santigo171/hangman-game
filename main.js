ad3bf81c8e4e5f7cac722cdd8bbfbb3a330fc4d5
David Hurtado <santibetancourt17@gmail.com>
2021-03-24 21:04:29 -0500

    Initial animation finished

--- main.js
+++ main.js
@@ -4,6 +4,44 @@
 // - save hangman games in local storage
 // - share hangman games with django
 
-const presentation = `
+// const changePerson_img = () => {new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         (true)
+//             ? resolve(() => {
+//                 if (i == 17) {
+//                     i = 0;
+//                 } else {
+//                     console.log('pichula')
+//                     persentation_img.setAttribute("src", `i${i}.jpg`);
+//                     i++;
+//                 }
+//             })
+//             : reject(new Error('There was an error'))
+//     }, 700)
+// })
+// };
 
-`
+const presentationButton_div = document.getElementById('presentation-button');
+const presentation_img = document.getElementById('img-hang');
+
+const presentation = ``;
+
+let currentStage_dom = "presentation";
+let i = 2;
+
+// Functions:
+
+function animatePresentation_img() {
+    setTimeout(() => {
+        if (i == 18) {
+            i = 1;
+        }
+        presentation_img.setAttribute("src", `./img/${i}.jpg`);
+        i++;
+        window.requestAnimationFrame(animatePresentation_img);
+    }, 500)
+}
+
+// Program
+
+animatePresentation_img();
